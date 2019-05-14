6ee44b7a1c24b709ed1b82b85dd372c15ee6a3db
720bit <49247495+720bit@users.noreply.github.com>
2019-05-14 12:14:51 -0500

    project for jammy

---
+++ DON'T TOUCH CODE1/FLIGHT CODE WORKING/Assets/SteamVR/Scripts/NewEnemyai.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewEnemyai : MonoBehaviour
+{
+    public GameObject Target;
+    public GameObject Bullet;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        Follow(Target);
+    }
+
+
+
+    public void Follow(GameObject Targ) //Follows the target(Typically the player
+    {
+        Vector3 Direction = Target.transform.position - gameObject.transform.position;
+        transform.LookAt(Target.transform);
+        Direction.Normalize();
+        Direction *= 0.01f;
+        gameObject.transform.position += Direction;
+    }
+
+    public void Shoot(GameObject Projectile)//Shoot projectiles at the player
+    {
+        Instantiate(Bullet, gameObject.transform);
+
+
+    }
+
+}
