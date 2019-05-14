a15df4801c3ebcd30152a4b32934d53e529f0a93
720bit <49247495+720bit@users.noreply.github.com>
2019-05-14 12:07:05 -0500

    enemyai

---
+++ SpiritBlastNew/DON'T TOUCH CODE/FLIGHT CODE WORKING/Assets/Scenes/EnemyAI.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour {
+    public GameObject Target;
+    public GameObject Bullet;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        Follow(Target);
+    }
+
+
+
+    public void Follow(GameObject Targ) //Follows the target(Typically the player
+    {
+        Vector3 Direction = Target.transform.position - gameObject.transform.position;
+        Direction.Normalize();
+        Direction *= 0.01f;
+        gameObject.transform.position += Direction;
+    }
+
+    public void Shoot(GameObject Projectile)//Shoot projectiles at the player
+    {
+        Instantiate(Bullet, gameObject.transform);
+
+
+    }
+
+}
