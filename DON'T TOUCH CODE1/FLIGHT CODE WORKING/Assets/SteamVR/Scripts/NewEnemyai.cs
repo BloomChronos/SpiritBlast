332e19dd9cbd81402dd602fa0c514cde8a181cb7
720bit <49247495+720bit@users.noreply.github.com>
2019-05-16 17:07:31 -0500

    f

--- DON'T TOUCH CODE1/FLIGHT CODE WORKING/Assets/SteamVR/Scripts/NewEnemyai.cs
+++ DON'T TOUCH CODE1/FLIGHT CODE WORKING/Assets/SteamVR/Scripts/NewEnemyai.cs
@@ -6,6 +6,7 @@ public class NewEnemyai : MonoBehaviour
 {
     public GameObject Target;
     public GameObject Bullet;
+    [SerializeField] private float speed;
     // Use this for initialization
     void Start()
     {
@@ -24,18 +25,21 @@ public class NewEnemyai : MonoBehaviour
 
     public void Follow(GameObject Targ) //Follows the target(Typically the player
     {
-        Vector3 Direction = Target.transform.position - gameObject.transform.position;
-        transform.LookAt(Target.transform);
-        Direction.Normalize();
-        Direction *= 0.01f;
-        gameObject.transform.position += Direction;
+         float step =  speed * Time.deltaTime; // calculate distance to move
+        transform.position = Vector3.MoveTowards(transform.position, Targ.transform.position, step);
+        //Vector3 Direction = Targ.transform.position - gameObject.transform.position;
+        transform.LookAt(Targ.transform);
+        //Direction.Normalize();
+        //Direction *= 0.01f;
+        //gameObject.transform.position += Direction;
+
     }
 
     public void Shoot(GameObject Projectile)//Shoot projectiles at the player
     {
         Instantiate(Bullet, gameObject.transform);
 
-
+        
     }
 
 }
